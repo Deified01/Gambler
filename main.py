89d5a4a4af3723edea91544e59ac3a135fa0a552
Deified01 <leviichi+2@hotmail.com>
2024-07-24 23:32:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ async def handle_message(event):
 
 async def send_gamble_task():
     while True:
-        await client.send_message(f'@lustXcatcherrobot', '/gamble 5000000000')
+        await client.send_message(f'@lustXcatcherrobot', '/gamble 100000000')
         logger.info("Sent /gamble 100")
         await asyncio.sleep(12.8)
 
